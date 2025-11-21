41f0d6e16634bf0efb514c8284472613c6ba8665
Piyush Sharma <piyushsharma0864@gmail.com>
2025-11-20 19:03:57 -0500

    make Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import LoginForm from './components/LoginForm.jsx';
+import Home from './components/Home.jsx';
 
 function App() {
   
   return (
     <>
-   <LoginForm />
+  <Home />
     </>
   )
 }
